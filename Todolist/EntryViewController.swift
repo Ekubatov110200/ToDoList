ca3d7d45deb85434a299e276b2c76ce287771b81
erzhan <kubatov_e@auca.kg>
2022-02-22 14:40:14 +0600

    Todolist finish

---
+++ Todolist/EntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EntryViewController.swift
+//  Todolist
+//
+//  Created by Kubatov Erzhan on 17.02.2022.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
